577e94c323bbd12b3ebadfb5d4a6eb2062d32d07
filomenogarcia2022 <filomenogarciacalisaya@gmail.com>
2022-05-01 21:02:09 -0500

    Subiendo actualizaciones...

--- usuarios2/panel/models.py
+++ usuarios2/panel/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class usuarios2(models.Model):
+    nombre=models.CharField(null=False, blank=False, max_length=25)
+    apellido=models.CharField(null=False, blank=False, max_length=25)
+    email=models.CharField(null=False, blank=False, max_length=25)
+
